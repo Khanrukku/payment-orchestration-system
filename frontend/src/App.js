126ad9f74e1aec77bf43e29779e9d37f987c9645
Rukaiya Khan <133051015+Khanrukku@users.noreply.github.com>
2025-10-15 02:11:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import MerchantList from './MerchantList';
 import './App.css';
 import { Activity, CreditCard, Users, BarChart3 } from 'lucide-react';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = "https://payment-orchestration-system.onrender.com";
 
 function App() {
   const [activeTab, setActiveTab] = useState('dashboard');
