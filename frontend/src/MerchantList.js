8327763af2b221e6ea58c12ea7c9ab5ab5cc33e8
Rukaiya Khan <133051015+Khanrukku@users.noreply.github.com>
2025-10-15 17:27:59 +0530

    Update base api  MerchantList.js

--- frontend/src/MerchantList.js
+++ frontend/src/MerchantList.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Users, Plus, Store, Mail, Key, CheckCircle, XCircle } from 'lucide-react';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = "https://payment-orchestration-system.onrender.com";
 
 function MerchantList({ merchants, onMerchantAdded }) {
   const [showForm, setShowForm] = useState(false);
